04367df5d44d0250deba92f9828c22eda67a687a
JoãoPedro <JooPedro@PC-Joao>
2017-03-18 19:33:14 -0300

    comentado código na classe de teste e alterado service js para pegar https

--- src/main/resources/public/services/versificacao_srvc.js
+++ src/main/resources/public/services/versificacao_srvc.js
@@ -2,7 +2,7 @@ angular.module("versificacao").factory("versificacao_srvc",function($http) {
 	porta = "8080";
 	
 	var _versificar = function(poematexto) {
-		return $http.get("http://localhost:" + porta +"/versificar?poema=" + poematexto);
+		return $http.get("https://localhost:" + porta +"/versificar?poema=" + poematexto);
 	}
 	
 	return {
