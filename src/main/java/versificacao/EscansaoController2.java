15ef0ef1e59c07973e47bbd304171165e34f99eb
jpontesmartins <jpontesmartins@gmail.com>
2017-03-19 17:01:10 -0300

    Update EscansaoController2.java

--- src/main/java/versificacao/EscansaoController2.java
+++ src/main/java/versificacao/EscansaoController2.java
@@ -5,9 +5,9 @@ import com.google.gson.Gson;
 public class EscansaoController2 {
 
     public String versificar(String exemplo1) {
-    	String caracteresA = "áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ";
+    	String caracteresA = "Ã¡Ã Ã£Ã¢Ã¤Ã©Ã¨ÃªÃ«Ã­Ã¬Ã®Ã¯Ã³Ã²ÃµÃ´Ã¶ÃºÃ¹Ã»Ã¼Ã§ÃÃ€ÃƒÃ‚Ã„Ã‰ÃˆÃŠÃ‹ÃÃŒÃÃÃ“Ã’Ã•Ã–Ã”ÃšÃ™Ã›ÃœÃ‡";
 		String caracteresB = "aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC";
-    	String versificado = "de/ re/ pen/ te/ não/ mais/ que/ de/ re/ pen/ te";
+    	String versificado = "de/ re/ pen/ te/ nÃ£o/ mais/ que/ de/ re/ pen/ te";
     	Poema poema = new Poema("de repente nao mais que de repente", versificado);
     	Gson gson = new Gson();
     	return gson.toJson(poema);
