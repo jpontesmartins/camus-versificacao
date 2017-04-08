45598dee76265c2a6fe1f2a658d2aa0a50229768
JoãoPedro <JooPedro@PC-Joao>
2017-04-08 08:40:47 -0300

    limpando o código

--- src/main/resources/public/services/versificacao_srvc.js
+++ src/main/resources/public/services/versificacao_srvc.js
@@ -1,27 +1,15 @@
 angular.module("versificacao").factory("versificacao_srvc",function($http) {
 	porta = "8080";
 	
-	var _versificar = function(poematexto) {
-		console.log('a');
-		user = "{}"
-	    return $http({
-	        method: 'POST',
-	        url: '/versificar',
-	        contentType: "application/json",
-	        data:user
-	    });
-	}
 	
 	var _versificar2 = function(poema){
-//	    var url = 'http://' + window.location.host;
 	    var url = window.location.origin;
-	    console.log('-> ' + window.location.origin);
+	    console.log('camus-versificação ' + poema);
 	    return $http.get(window.location.origin + "/versificar?poemaTexto=" + poema);
 	};
 	
 	
 	return {
-		versificar : _versificar,
 		versificar2 : _versificar2
 	}
 });
