2520ad8c6de6caad42dde4e78104a67b9ff244ab
JoãoPedro <JooPedro@PC-Joao>
2017-06-18 10:37:03 -0300

    atribuição de peso na aliteração e digrafos

--- src/test/java/versificacao/figurasdelinguagem/AliteracaoTest.java
+++ src/test/java/versificacao/figurasdelinguagem/AliteracaoTest.java
@@ -9,7 +9,7 @@ public class AliteracaoTest {
 	public void fraseComRMaisFrequenteTest() {
 		Aliteracao figuras = new Aliteracao();
 		String frase = "o rato roeu a roupa do rei de roma";
-		Assert.assertEquals(5, figuras.descobrirConsoantesMaisFrequentes(frase).get("r").intValue());
+		Assert.assertEquals(10, figuras.descobrirConsoantesMaisFrequentes(frase).get("r").intValue());
 
 	}
 	
@@ -17,7 +17,7 @@ public class AliteracaoTest {
 	public void fraseComVEZMaisFrequentesTest() {
 		Aliteracao figuras = new Aliteracao();
 		String frase = "vozes veladas, veludosas vozes";
-		Assert.assertEquals(4, figuras.descobrirConsoantesMaisFrequentes(frase).get("v").intValue());
+		Assert.assertEquals(8, figuras.descobrirConsoantesMaisFrequentes(frase).get("v").intValue());
 		Assert.assertEquals(5, figuras.descobrirConsoantesMaisFrequentes(frase).get("s").intValue());
 		Assert.assertEquals(2, figuras.descobrirConsoantesMaisFrequentes(frase).get("z").intValue());
 
@@ -27,8 +27,8 @@ public class AliteracaoTest {
 	public void fraseComVEDMaisFrequentesTest() {
 		Aliteracao figuras = new Aliteracao();
 		String frase = "quem me vê apanhando da vida duvida que eu vá revidar";
-		Assert.assertEquals(6, figuras.descobrirConsoantesMaisFrequentes(frase).get("d").intValue());
-		Assert.assertEquals(5, figuras.descobrirConsoantesMaisFrequentes(frase).get("v").intValue());
+		Assert.assertEquals(8, figuras.descobrirConsoantesMaisFrequentes(frase).get("d").intValue());
+		Assert.assertEquals(8, figuras.descobrirConsoantesMaisFrequentes(frase).get("v").intValue());
 	}
 
 }
