5db5309e408c6409de1b0f4c03560cba9e5a97cb
jpontesmartins <jpontesmartins@gmail.com>
2018-01-06 14:35:49 -0200

    refatoração do teste

--- src/main/java/camus/lacan/domain/AnaliseAnafora.java
+++ src/main/java/camus/lacan/domain/AnaliseAnafora.java
@@ -1,22 +1,53 @@
 package camus.lacan.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnaliseAnafora {
 
-	public boolean temAnaforaNosVersos(String... versos) {
+	private List<List<String>> repeticoes = new ArrayList<>();
+	private List<String> versos;
 
-		String primeiroVerso = versos[0];
-		String segundoVerso = versos[1];
-
-		String[] palavrasDoPrimeiroVerso = primeiroVerso.split(" ");
-		String[] palavrasDoSegundoVerso = segundoVerso.split(" ");
-
-		if (palavrasDoPrimeiroVerso.length > 0 && palavrasDoSegundoVerso.length > 0) {
-			if (palavrasDoPrimeiroVerso[0].equalsIgnoreCase(palavrasDoSegundoVerso[0])) {
-				return true;
-			}
+	public boolean temAnaforaNosVersos(List<String> versos) {
+		this.versos = versos;
+		for (int posicaoVersoAtual = 0; posicaoVersoAtual < versos.size() - 1; posicaoVersoAtual++) {
+			compararComVersoSeguinte(posicaoVersoAtual);
 		}
-
-		return false;
+		mostrarRepeticoes();
+		if (repeticoes.isEmpty()) return false;
+		return true;
 	}
 
+	private void mostrarRepeticoes() {
+		for (List<String> lista : repeticoes) {
+			lista.forEach(palavra -> System.out.println(palavra));
+			System.out.println("----");
+		}
+	}
+
+	private boolean compararComVersoSeguinte(int posicaoVersoAtual) {
+
+		String versoAtual = versos.get(posicaoVersoAtual);
+		String versoSeguinte = versos.get(posicaoVersoAtual + 1);
+
+		String[] palavrasDoVersoAtual = versoAtual.split(" ");
+		String[] palavrasDoVersoSeguinte = versoSeguinte.split(" ");
+
+		int tamanhoDoMenorVerso = palavrasDoVersoAtual.length;
+		if (palavrasDoVersoSeguinte.length < tamanhoDoMenorVerso) {
+			tamanhoDoMenorVerso = palavrasDoVersoSeguinte.length;
+		}
+
+		List<String> palavrasRepetidas = new ArrayList<>();
+		for (int i = 0; i < tamanhoDoMenorVerso; i++) {
+			if (palavrasDoVersoAtual[i].equalsIgnoreCase(palavrasDoVersoSeguinte[i])) {
+				palavrasRepetidas.add(palavrasDoVersoAtual[i]);
+			}
+		}
+		if (!palavrasRepetidas.isEmpty()) {
+			repeticoes.add(palavrasRepetidas);
+		}
+		return true;
+
+	}
 }
