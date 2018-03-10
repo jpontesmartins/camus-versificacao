125f78bc6a09376aea055e47bc54cbe71db0b1de
jpontesmartins <jpontesmartins@gmail.com>
2018-03-10 13:06:39 -0300

    ajuste pro quality-gate do codacy

--- src/test/java/camus/lacan/domain/tonicidade/TonicidadeServiceTest.java
+++ src/test/java/camus/lacan/domain/tonicidade/TonicidadeServiceTest.java
@@ -1,10 +1,5 @@
 package camus.lacan.domain.tonicidade;
 
 public class TonicidadeServiceTest {
-	
-	
-	public void teste() {
-		TonicidadeService tonicidadeService = new TonicidadeService();
-	}
 
 }
