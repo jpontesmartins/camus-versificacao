438a84513fe145feaa5dd33098cce93a8698e366
jpontesmartins <jpontesmartins@gmail.com>
2018-03-11 09:09:32 -0300

    refatoração service test

--- src/main/java/camus/lacan/domain/tonicidade/TonicidadeService.java
+++ src/main/java/camus/lacan/domain/tonicidade/TonicidadeService.java
@@ -1,14 +1,14 @@
 package camus.lacan.domain.tonicidade;
 
 import camus.lacan.domain.Palavra;
-import camus.lacan.domain.tonicidade.monossilabos.Tonicidade;
+import camus.lacan.domain.tonicidade.monossilabos.AnaliseTonicidade;
 
 public class TonicidadeService implements ITonicidadeService {
 	
 
 	public Palavra encontrarSilabaTonica(String palavra) {
 		Palavra vocabulo = new Palavra();
-		Tonicidade tonicidade = new Tonicidade();
+		AnaliseTonicidade tonicidade = new AnaliseTonicidade();
 		vocabulo.setGrafia(palavra);
 		vocabulo.setSilabaTonica(tonicidade.encontrarSilabaTonica(palavra));
 		vocabulo.setTonicidade(TonicidadeEnum.descricao(tonicidade.encontrarTonicidadeDaPalavra(palavra)));
